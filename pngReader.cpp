47b7414b05de70bded0fe58d328a608c6496d393
Samu698 <gpg@cerea.dev>
2022-07-31 17:29:35 +0200

    pngReader reads file only when calling getter

--- pngReader.cpp
+++ pngReader.cpp
@@ -1,24 +1,37 @@
 #include "pngReader.hpp"
-PngReader::PngReader(const fs::path& path) {
-	FILE* fp = fopen(path.c_str(), "rb");
+#include <png.h>
+PngReader::PngReader(const fs::path& path) : path(path), infoRead(false) {}
+
+void PngReader::getImgInfo() {
+	if (infoRead) return;
+
+	fp = fopen(path.c_str(), "rb");
 	uint8_t header[8];
 	if (!fp) throw std::invalid_argument("cannot open file");
 	fread(header, 8, 1, fp);
 	if (png_sig_cmp(header, 0, 8)) throw std::invalid_argument("file is not a PNG");
 
-	png_struct* png = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+	png = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
 	if (!png) throw std::runtime_error("png_create_read_struct failed");
-	png_info* info = png_create_info_struct(png);
+	info = png_create_info_struct(png);
 	if (!info) throw std::runtime_error("png_create_info_struct failed");
 
 	png_init_io(png, fp);
 	png_set_sig_bytes(png, 8);
 	
 	png_read_info(png, info);
-	w = png_get_image_width(png, info);
-	h = png_get_image_height(png, info);
-	colorType = png_get_color_type(png, info);
-	depth = png_get_bit_depth(png, info);
+
+	infoRead = true;
+}
+
+void PngReader::readPixels() {	
+	getImgInfo();
+	if (!pixels.empty()) return;
+
+	const uint32_t w = png_get_image_width(png, info);
+	const uint32_t h = png_get_image_height(png, info);
+	const uint8_t colorType = png_get_color_type(png, info);
+	const uint8_t depth = png_get_bit_depth(png, info);
 
 	if (depth == 16) png_set_strip_16(png);
 
@@ -34,13 +47,19 @@ PngReader::PngReader(const fs::path& path) {
 	uint8_t* rows[h];
 	for (uint32_t y = 0; y < h; y++) rows[y] = pixels.data() + rowbytes * y;
 	png_read_image(png, rows);
+}
 
+PngReader::~PngReader() {
 	png_destroy_read_struct(&png, &info, nullptr);
 	fclose(fp);
 }
 
-const std::vector<uint8_t>& PngReader::getPixels() const { return pixels; }
-std::vector<uint8_t> PngReader::getScaledPixels(uint32_t newW, uint32_t newH) const {
+const std::vector<uint8_t>& PngReader::getPixels() { readPixels(); return pixels; }
+std::vector<uint8_t> PngReader::getScaledPixels(uint32_t newW, uint32_t newH) {
+	readPixels();
+	const uint32_t w = png_get_image_width(png, info);
+	const uint32_t h = png_get_image_height(png, info);
+
 	if (newW == w && newH == h) return pixels;	
 
 	float xratio = (float)w / newW;
@@ -60,7 +79,7 @@ std::vector<uint8_t> PngReader::getScaledPixels(uint32_t newW, uint32_t newH) co
 	return scaled;
 }
 
-uint32_t PngReader::getWidth() const { return w; }
-uint32_t PngReader::getHeight() const { return h; }
-uint8_t PngReader::getDepth() const { return depth; }
-uint8_t PngReader::getColorType() const { return colorType; }
+uint32_t PngReader::getWidth() { getImgInfo(); return png_get_image_width(png, info); }
+uint32_t PngReader::getHeight() { getImgInfo(); return png_get_image_height(png, info); }
+uint8_t PngReader::getDepth() { getImgInfo(); return png_get_bit_depth(png, info); }
+uint8_t PngReader::getColorType() { getImgInfo(); return png_get_color_type(png, info); }
--- pngReader.hpp
+++ pngReader.hpp
@@ -4,17 +4,23 @@
 #include "utils.hpp"
 
 class PngReader {
+	fs::path path;
 	std::vector<uint8_t> pixels;
-	uint32_t w, h;
-	uint8_t colorType, depth;
+	FILE* fp;
+	png_struct* png;
+	png_info* info;
+	bool infoRead;
 
+	void getImgInfo();
+	void readPixels();
 public:
 	PngReader(const fs::path& path);
+	~PngReader();
 
-	const std::vector<uint8_t>& getPixels() const;
-	std::vector<uint8_t> getScaledPixels(uint32_t newW, uint32_t newH) const;
-	uint32_t getWidth() const;
-	uint32_t getHeight() const;
-	uint8_t getDepth() const;
-	uint8_t getColorType() const;
+	const std::vector<uint8_t>& getPixels();
+	std::vector<uint8_t> getScaledPixels(uint32_t newW, uint32_t newH);
+	uint32_t getWidth();
+	uint32_t getHeight();
+	uint8_t getDepth();
+	uint8_t getColorType();
 };
